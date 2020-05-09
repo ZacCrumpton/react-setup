b9568b60a24359e62abe287290c5591ba8ccf512
Zac Crumpton <z.crumpton@gmail.com>
2020-05-09 11:14:42 -0500

    setup

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>INSIDE THE APP COMPOUND DAWG</h1>
+        <button className="btn btn-dark">TEST HOMIE</button>
+      </div>
+    );
+  }
+}
+
+export default App;
